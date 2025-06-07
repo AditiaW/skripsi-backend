875b4fbd93ba6e4dcd69ddf4ffc953f2066dc522
Aditia Winanda <aditiaw1507@gmail.com>
2025-06-07 18:31:36 +0700

    Fixed CORS handling: dynamically support multiple origins

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,23 @@ const app = express();
 const port = process.env.PORT
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 100, 
+  max: 100,
   message: "Too many requests, please try again later.",
 });
 
+const allowedOrigins = process.env.ORIGIN.split(",").map(origin => origin.trim());
+
 app.use(cors({
-    origin: process.env.ORIGIN,
-    credentials: true,
-}))
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 app.use(compression())
 app.use(cookieParser())
@@ -27,5 +36,5 @@ app.use(limiter);
 app.use('/', routes)
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on http://localhost:${port}`)
 })
