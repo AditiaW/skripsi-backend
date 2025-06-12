fc9962a09854c70bdb26c4de09b9dd16fe03f66f
Aditia Winanda <aditiaw1507@gmail.com>
2025-06-12 23:16:47 +0700

    feat(auth): tambahkan field name ke dalam token JWT

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -307,7 +307,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
             return;
         }
         const token = jwt.sign(
-            { id: user.id, email: user.email, role: user.role },
+            { id: user.id, name: user.name, email: user.email, role: user.role },
             process.env.JWT_SECRET!,
             { expiresIn: "1y" }
         );
